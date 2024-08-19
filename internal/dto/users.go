36bf5430f78c62ea88890f87dca8a1fefeaca3ca
Evgeniy <Z_shevelev@mail.ru>
2024-08-19 04:19:38 +0300

    add validate fields to user dto

--- internal/dto/users.go
+++ internal/dto/users.go
@@ -1,9 +1,9 @@
 package dto
 
 type User struct {
-	Name    string  `json:"name" validate:"required"`
-	Skill   float64 `json:"skill" validate:"required"`
-	Latency float64 `json:"latency" validate:"required"`
+	Name    string  `json:"name" validate:"required,regexp=^[a-zA-Z_]+$"`
+	Skill   float64 `json:"skill" validate:"required,gt=0,lt=11"`
+	Latency float64 `json:"latency" validate:"required,gt=0,lt=11"`
 }
 
 type UserDistance struct {
